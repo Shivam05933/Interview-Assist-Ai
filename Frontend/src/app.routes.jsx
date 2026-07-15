49a58a1c09eacbff0ec1b2dd87f06e3ebbe39982
Shivam05933 <shivam865593@gmail.com>
2026-07-15 22:03:36 +0530

    add protected again at home

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -17,7 +17,7 @@ export const router = createBrowserRouter([
     },
     {
         path: "/",
-        element: <Home />
+        element: <Protected><Home /></Protected>
     },
     {
         path:"/interview/:interviewId",
